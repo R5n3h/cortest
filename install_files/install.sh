7b54368006121aa32384b373d34b7f29a913358d
Ron Sneh <me@ronsneh.com>
2016-05-30 14:48:37 +0300

    update install files

--- install_files/install.sh
+++ install_files/install.sh
@@ -5,8 +5,10 @@ BASE_DIR="/vagrant/"
 INSTALL_DIR="install_files"
 
 cd $BASE_DIR
-cp "$INSTALL_DIR/coretestserver" "/etc/nginx/sites-enabled/"
+cp "$INSTALL_DIR/cortestserver" "/etc/nginx/sites-enabled/"
 cp "$INSTALL_DIR/cortestapp.conf" "/etc/init/"
+
+useradd -G www-data web
 
 /etc/init.d/nginx restart
 service cortestapp start
