5456e2fcd0bf5d0f2e8a74ad9de7dd1c359ebb6b
Ron Sneh <me@ronsneh.com>
2016-05-30 14:38:36 +0300

    update install script

--- install_files/install.sh
+++ install_files/install.sh
@@ -5,8 +5,8 @@ BASE_DIR="/vagrant/"
 INSTALL_DIR="install_files"
 
 cd $BASE_DIR
-cp $INSTALL_DIR/coretestserver /etc/nginx/sites-enabled/
-cp $INSTALL_DIR/cortestapp.conf /etc/init/
+cp "$INSTALL_DIR/coretestserver" "/etc/nginx/sites-enabled/"
+cp "$INSTALL_DIR/cortestapp.conf" "/etc/init/"
 
 /etc/init.d/nginx restart
 service cortestapp start
